7eef2b7d5bb0da464799527cb5ac1b4c59e5cdf5
Sahas Ramesh <sahasramesh22@gmail.com>
2019-08-25 00:30:25 -0700

    added gui, optimized some functions, updated some words in everyword.

--- word_finder.py
+++ word_finder.py
@@ -1,120 +1,140 @@
+#import GUI module
+import PySimpleGUI as sg
+
 #import permutations module and timer
 import time
 from itertools import permutations
-
-#define counter and empty lists
-ctr = 0
-wordCombo = []
-wordLet = []
-longCtr = 0
-filterWords = []
-finalList = []
-comboPerms = []
-allPerms = []
-
-#removes duplicates from list
-def noRepeats(x):
-  return list(dict.fromkeys(x))
-
-#function to convert list to string
-def convert(list):
-    s = [str(i) for i in list]
-    res = str(", ".join(s))
-    print(res)
-
-#convert list of letters into strings
-def convertLet(list):
-    s = [str(i) for i in list]
-    res = str("".join(s))
-    print(res)
-
-#function to remove 2 and 1 letter words
-def removeShortPerms(wordss):
-    return [i for i in wordss if len(i) >= 3]
-
-#get user input
-userLet = input("Enter letters(up to 8):")
 start_time = time.time()
-userLen = len(userLet)
-if userLen > 8:
-    print("Too many letters!\n")
-    exit()
 
-#open words doc and split words into list
-with open("everyword.rtf") as f:
-    words = f.read().split()
-words = [x.lower() for x in words]
-wordsLen = len(words)
+def WordScrape(userLet):
+    #define counter and empty lists
+    ctr = 0
+    wordCombo = []
+    wordLet = []
+    longCtr = 0
+    filterWords = []
+    finalList = []
+    comboPerms = []
+    allPerms = []
 
-#find all permutations of user input
-perms = [''.join(p) for p in permutations(userLet)]
-permsLen = len(perms)
+    #removes duplicates from list
+    def noRepeats(x):
+        return list(dict.fromkeys(x))
 
-print("\nLoading permutations...")
-#print("All permutations: ", perms)
+    #function to convert list to string
+    def convert(list):
+        s = [str(i) for i in list]
+        res = str(", ".join(s))
+        return res
 
-#filter out words longer than the user-submitted letters(or too short)
-for z in range(0, wordsLen):
-    if 3 <= len(words[z]) <= userLen:
-        filterWords.append(words[z])
-filterLen = len(filterWords)
+    #convert list of letters into strings
+    def convertLet(list):
+        s = [str(i) for i in list]
+        res = str("".join(s))
+        return res
 
-#create smaller permutations
-def shorterPerms(num):
-    newPerms = []
-    for y in range(0, permsLen):
-        miniPerm = perms[y]
-        miniPerm = miniPerm[num : : ]
-        newPerms.append(miniPerm)
-    return newPerms
+    #function to remove 2 and 1 letter words
+    def removeShortPerms(wordss):
+        return [i for i in wordss if len(i) >= 3]
 
-#add all perm lists depending on number of letters
-if userLen == 1:
-    allPerms = perms
+    #interpret user input
+    userLen = len(userLet)
 
-if userLen == 2:
-    allPerms = perms
+    #open words doc and split words into list
+    with open("everyword.rtf") as f:
+        words = f.read().split()
+    words = [x.lower() for x in words]
+    wordsLen = len(words)
 
-if userLen == 3:
-    allPerms = perms
+    #find all permutations of user input
+    perms = [''.join(p) for p in permutations(userLet)]
+    permsLen = len(perms)
 
-if userLen == 4:
-    for i in range(1, userLen):
-        comboPerms+= list(dict.fromkeys(shorterPerms(i)))
-    allPerms = perms + comboPerms
+    print("\nLoading permutations...")
+    #print("All permutations: ", perms)
 
-if userLen == 5:
-    for i in range(1, userLen):
-        comboPerms+= list(dict.fromkeys(shorterPerms(i)))
-    print(comboPerms)
-    allPerms = perms + comboPerms
+    #filter out words longer than the user-submitted letters(or too short)
+    for z in range(0, wordsLen):
+        if 3 <= len(words[z]) <= userLen:
+            filterWords.append(words[z])
+    filterLen = len(filterWords)
 
-if userLen == 6:
-    for i in range(1, userLen):
-        comboPerms+= list(dict.fromkeys(shorterPerms(i)))
-    allPerms = perms + comboPerms
+    #create smaller permutations
+    def shorterPerms(num):
+        newPerms = []
+        for y in range(0, permsLen):
+            miniPerm = perms[y]
+            miniPerm = miniPerm[num : : ]
+            newPerms.append(miniPerm)
+        return newPerms
 
-if userLen == 7:
-    for i in range(1, userLen):
-        comboPerms+= list(dict.fromkeys(shorterPerms(i)))
-    allPerms = perms + comboPerms
+    #add all perm lists depending on number of letters
+    if userLen == 1:
+        allPerms = perms
 
-if userLen == 8:
-    for i in range(1, userLen):
-        comboPerms+= list(dict.fromkeys(shorterPerms(i)))
-    allPerms = perms + comboPerms
+    if userLen == 2:
+        allPerms = perms
 
-allPerms = list(dict.fromkeys(allPerms))
-allPerms = removeShortPerms(allPerms)
-allPermsLen = len(allPerms)
+    if userLen == 3:
+        allPerms = perms
 
-#find match with perms from word doc
-print("Finding matches...\n")
-for x in range(0, allPermsLen):
-    for y in range(0, filterLen):
-        if allPerms[x] == filterWords[y]:
-            finalList.append(allPerms[x])
+    if userLen == 4:
+        for i in range(1, userLen):
+            comboPerms+= list(dict.fromkeys(shorterPerms(i)))
+        allPerms = perms + comboPerms
 
-finalWords = str(convert(noRepeats(finalList)))
+    if userLen == 5:
+        for i in range(1, userLen):
+            comboPerms+= list(dict.fromkeys(shorterPerms(i)))
+        allPerms = perms + comboPerms
+
+    if userLen == 6:
+        for i in range(1, userLen):
+            comboPerms+= list(dict.fromkeys(shorterPerms(i)))
+        allPerms = perms + comboPerms
+
+    if userLen == 7:
+        for i in range(1, userLen):
+            comboPerms+= list(dict.fromkeys(shorterPerms(i)))
+        allPerms = perms + comboPerms
+
+    if userLen == 8:
+        for i in range(1, userLen):
+            comboPerms+= list(dict.fromkeys(shorterPerms(i)))
+        allPerms = perms + comboPerms
+
+    allPerms = list(dict.fromkeys(allPerms))
+    allPerms = removeShortPerms(allPerms)
+    allPermsLen = len(allPerms)
+
+    #find match with perms from word doc
+    print("Finding matches...\n")
+    for x in range(0, allPermsLen):
+        for y in range(0, filterLen):
+            if allPerms[x] == filterWords[y]:
+                finalList.append(allPerms[x])
+
+    return str(convert(noRepeats(finalList)))
+
+#implelement GUI
+layout = [
+    [sg.Text('Enter your letters:', size=(15, 1)), sg.InputText()],
+    [sg.Text('Words:'), sg.Text('', size=(150,1), key='_OUTPUT_')],
+    [sg.Submit(), sg.Exit()]
+]
+
+window = sg.Window('WordScrape', layout)
+
+while True:
+    event, values = window.Read()
+    print(event, values)
+    if event is None or event == 'Exit':
+        break
+    if event == 'Submit':
+        window.Element('_OUTPUT_').Update(str(WordScrape(values[0])))
+
+window.Close()
+
+print(values[0])
 print("\n")
 print ("My program took", time.time() - start_time, "seconds to run\n")
